7890baca7b442911e896ea315fe654d925ab58c0
SionNoh <vmfpsel@gmail.com>
2024-03-11 03:04:26 +0900

    setup

--- basic/src/AppCounter.jsx
+++ basic/src/AppCounter.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
 export default function AppCounter() {
+    let [score, setScore] = useState(0);
     return (
         <div>
-            <Counter />
+            <span className='total-count'>Total Count: {score} {score > 10 ? "★":"☆"}</span>
+            <Counter setScore={setScore}/>
+            <Counter setScore={setScore}/>
         </div>
     );
 }
--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 
-export default function Counter() {
+export default function Counter({setScore}) {
     const [count, setCount] = useState(0);
     return (
-        <div className='counter'>
-            <span className='number'>{ count }</span>
-            <button className='button' onClick={()=>{
-                setCount(count+1);
-            }}>Add +</button>
+        <div className='container'>
+            <div className='counter'>
+                <span className='number'>{ count }</span>
+                <button className='button' onClick={()=>{
+                    setCount((prev)=>prev+1);
+                    setScore((prev)=>prev+1);
+                }}>Add +</button>
+            </div>
         </div>
     );
 }
