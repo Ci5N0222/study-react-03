eee834bb53b4695fdd8fcd03492c6468241599fe
SionNoh <vmfpsel@gmail.com>
2024-03-11 01:10:02 +0900

    setup

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+/**
+ * 중요
+ * 1. 모든 컴포넌트는 1개의 테그를 반환해야 한다 (두개 이상의 태그가 병렬로 반환되면 안됨)
+ * 2. 변수 사용 법 : { 변수 }
+ */
+
 function App() {
+  const name = "Noh"
   return (
     <div className="App">
       <h1 className='orange'>Hello!</h1>
+      <h2>I'm { name }</h2>
     </div>
   );
 }
