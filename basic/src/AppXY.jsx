7c85ea15eca755d327c7e75127edfd46dd938445
SionNoh <vmfpsel@gmail.com>
2024-03-11 15:41:52 +0900

    basic study

---
+++ basic/src/AppXY.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AppXY.css';
+
+export default function AppXY() {
+    return (
+        <div className='container'>
+            <div className='pointer' />
+        </div>
+    );
+}
--- basic/src/index.js
+++ basic/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppProfile from './AppProfile'
-import AppCounter from './AppCounter';
 import reportWebVitals from './reportWebVitals';
-import AppProducts from './AppProducts';
+import AppXY from './AppXY';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <AppXY />
   </React.StrictMode>
 );
 
