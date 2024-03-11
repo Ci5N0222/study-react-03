927e70b7c1194cf2d0a9269725a92b1a40cccd19
SionNoh <vmfpsel@gmail.com>
2024-03-11 15:50:08 +0900

    pointer event test

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
     return (
-        <div className='container'>
-            <div className='pointer' />
+        <div className='container' onPointerMove={(e)=>{
+            console.log(e.clientX, e.clientY);
+            setX(e.clientX);
+            setY(e.clientY);
+        }}>
+            <div className='pointer' style={{transform: `translate(${x}px, ${y}px)`}}/>
         </div>
     );
 }
