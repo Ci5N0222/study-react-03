e05b5dbc2514ca4c62d631739571d3201b15abb8
SionNoh <vmfpsel@gmail.com>
2024-03-13 01:44:09 +0900

    Use PostCss

--- css/src/AppPostCss.jsx
+++ css/src/AppPostCss.jsx
@@ -4,6 +4,17 @@ import Button2 from './components/Button2';
 
 /**
  * PostCSS
+ * 1. 파일 이름
+ *  - css 확장자 전에 module을 붙여준다.
+ *  - 예시 : Button1.module.css
+ * 
+ * 2. 임포트
+ *  - import 시 사용하고자 하는 변수명을 설정한다.
+ *  - 예시 : import styles from './Button2.module.css'
+ * 
+ * 3. 사용
+ *  - 사용시 {} 안에 import된 변수명.클래스명으로 사용
+ *  - 예시 : <button className={styles.button}>Button1</button>
  */
 
 export default function AppPostCss() {
--- css/src/components/Button1.jsx
+++ css/src/components/Button1.jsx
@@ -4,7 +4,7 @@ import styles from './Button1.module.css'
 export default function Button1() {
     return (
         <>
-        <button className={styles.button}>Button1</button>
+            <button className={styles.button}>Button1</button>
         </>
     );
 }
