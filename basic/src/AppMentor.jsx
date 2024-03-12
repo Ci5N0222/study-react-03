6b36cfd718e06b44a724356a70e7fbd05b9ca3bc
SionNoh <vmfpsel@gmail.com>
2024-03-12 21:59:34 +0900

    setup

--- basic/src/AppMentor.jsx
+++ basic/src/AppMentor.jsx
@@ -15,9 +15,16 @@ export default function AppMentor() {
             <p>{person.name}의 멘토는 {person.mentor.name} ({person.mentor.title})</p>
             <button onClick={()=> {
                 const name = prompt(`what's your mentor's name?`);
+                let copy = {...person};
+                copy.mentor.name = name;
+                setPerson(copy);
             }}>멘토 이름 바꾸기</button>
             <button onClick={()=> {
-                const name = prompt(`what's your mentor's title?`);
+                const title = prompt(`what's your mentor's title?`);
+                setPerson((person)=>({
+                    ...person, 
+                    mentor: {...person.mentor, title}
+                }));
             }}>멘토 타이틀 바꾸기</button>
         </div>
     );
