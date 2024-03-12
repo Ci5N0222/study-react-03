953b957bbb734c58bf4f78a6a3f795783894e83c
SionNoh <vmfpsel@gmail.com>
2024-03-12 21:38:34 +0900

    setup

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
+    const [position, setPosition] = useState({x:0, y:0});
     return (
         <div className='container' onPointerMove={(e)=>{
             console.log(e.clientX, e.clientY);
-            setX(e.clientX);
-            setY(e.clientY);
+            setPosition({x:e.clientX, y:e.clientY});
         }}>
-            <div className='pointer' style={{transform: `translate(${x}px, ${y}px)`}}/>
+            <div 
+                className='pointer' 
+                style={{transform: `translate(${position.x}px, ${position.y}px)`}}/>
         </div>
     );
 }
