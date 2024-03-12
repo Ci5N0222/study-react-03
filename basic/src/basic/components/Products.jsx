1a42f41eb17c7789272765c6cfd9d2dc1be867cc
SionNoh <vmfpsel@gmail.com>
2024-03-13 01:01:13 +0900

    loading, error setting

--- basic/src/basic/components/Products.jsx
+++ basic/src/basic/components/Products.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
 
     const [products, setProducts] = useState([]);
     const [checked, setChecked] = useState(false);
@@ -9,16 +11,23 @@ export default function Products() {
     }
 
     useEffect(()=>{
+        setLoading(true);
+        setError(undefined);
         fetch(`data/${checked ? 'sale_':''}products.json`)
         .then(res => res.json())
         .then(data=>{
             console.log("네트워크에서 데이터를 받아옴");
             setProducts(data);
-        });
+        })
+        .catch((e) => setError('Error 발생!!'))
+        .finally(()=> setLoading(false));
         return ()=>{
             console.log('깨끗하게 청소하는 일을 합니다.')
         }
     }, [checked]);
+
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>{error}</p>;
 
     return (
         <>
--- basic/src/index.js
+++ basic/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppMentorsButton from './AppMentorsButton';
+import AppProducts from './basic/AppProducts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentorsButton />
+    <AppProducts />
   </React.StrictMode>
 );
 
