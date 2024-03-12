ec9bf070d24edf2d5d4f10c20d0555c94a659f42
SionNoh <vmfpsel@gmail.com>
2024-03-13 01:13:54 +0900

    custom hook test

--- basic/src/basic/components/Products.jsx
+++ basic/src/basic/components/Products.jsx
@@ -1,30 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import useProducts from '../../hooks/use-products';
 
 export default function Products() {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
-
-    const [products, setProducts] = useState([]);
     const [checked, setChecked] = useState(false);
+    const [loading, error, products] = useProducts({ salesOnly: checked })
     const handleChange = () => {
         setChecked((prev) => !prev);
     }
-
-    useEffect(()=>{
-        setLoading(true);
-        setError(undefined);
-        fetch(`data/${checked ? 'sale_':''}products.json`)
-        .then(res => res.json())
-        .then(data=>{
-            console.log("네트워크에서 데이터를 받아옴");
-            setProducts(data);
-        })
-        .catch((e) => setError('Error 발생!!'))
-        .finally(()=> setLoading(false));
-        return ()=>{
-            console.log('깨끗하게 청소하는 일을 합니다.')
-        }
-    }, [checked]);
 
     if(loading) return <p>Loading...</p>;
     if(error) return <p>{error}</p>;
