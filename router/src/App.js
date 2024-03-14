f2dcb7d52b1c19bcb00ae8f89275ec6b714b6781
SionNoh <vmfpsel@gmail.com>
2024-03-14 16:12:14 +0900

    router setup

--- router/src/App.js
+++ router/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <p>Home</p>,
+    errorElement: <p>Not Found!!</p>
+  },
+  {
+    path: '/videos',
+    element: <p>Video</p>
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={ router } />
   );
 }
 
